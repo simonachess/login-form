a6ef89c81fdc39675ad680396d2613dd33e62dbc
simonachess <simonachess@gmail.com>
2021-12-08 11:32:39 +0200

    login success failed alert

---
+++ src/components/Login.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Alert } from "react-bootstrap";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    // const [responseFetch, setResponseFetch] = ([]);
+    const [alert, setAlert] = useState(false);
+    const [response, setResponse] = useState({
+        data: '',
+        type: 'success',
+    });
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        postData();
+    }
+
+    async function postData() {
+        const newRecord = {
+            email: email,
+            password: password,
+        };
+        try {
+            const res = await fetch('https://dev.api.globetrott.app/api/users/login/', {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newRecord),
+            });
+
+            const fetchData = await res.json();
+            console.log(fetchData)
+
+            // console.log(res);
+
+            if (res.ok) {
+                setResponse({ data: 'Logged in successfully', type: 'success' });
+                showAlert(true);
+            }
+            if (!res.ok) {
+                setResponse({
+                    data: 'Ups... Something went wrong',
+                    type: 'error'
+                });
+                showAlert(true);
+            }
+
+        } catch (err) {
+            console.log('Error: ' + err);
+            setResponse({
+                data: `Ups... Something went wrong CATCH ${err}`,
+                type: 'error'
+            });
+            showAlert(true);
+        }
+    }
+
+    const showAlert = (status) => {
+        setAlert(status)
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            showAlert();
+        }, 4000);
+    }, [alert]);
+
+    return (
+        <div className="Login">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button size="lg" type="submit" className="login-button" disabled={!validateForm()}>
+                    Login
+                </Button>
+            </Form>
+            {alert ? <Alert variant={`${(response.type === 'success') ? 'success' : 'danger'}`}>{response.data}</Alert> : ''}
+        </div>
+    );
+}
+
+// Email: cct-lab@cct.lt
+// Password: cctLAB123@
